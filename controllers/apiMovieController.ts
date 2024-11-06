cd3ac505d604a43d3141ac24c733d1dbb74ddad0
AriyanMlzm <ryanmolazem@gmail.com>
2024-11-06 12:58:33 +0330

    add mongo

--- controllers/apiMovieController.ts
+++ controllers/apiMovieController.ts
@@ -1,7 +1,17 @@
 import { Response, Request } from 'express'
 
-const apiMovieController = (_: Request, res: Response) => {
-	res.render('index')
+const apiCreateController = async (req: Request, res: Response) => {
+	const { name, year, type } = req.body
+	const response = await fetch(
+		`${process.env.OMDB_URL}&t=${name}&type=${type}&y=${year}`
+	)
+	const data = await response.json()
+
+	if (data.Response === 'False') {
+		res.status(404).send('Not found!')
+	} else if (data.Response === 'True') {
+		res.status(200).send(data)
+	}
 }
 
-export default apiMovieController
+export default apiCreateController
--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import('./configs/db')
 
 import { homeRouter, apiRouter } from './routes'
 
+dotenv.config()
 const app = express()
 
-const port = 4000
+const port = process.env.PORT || 4000
 app.set('view engine', 'ejs')
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(express.static('public'))
 
 app.use('/', homeRouter)
