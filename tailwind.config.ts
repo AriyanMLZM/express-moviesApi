62e1d708cd916f3ae73310731bbfd9d03aced8e5
AriyanMlzm <ryanmolazem@gmail.com>
2024-11-09 12:38:59 +0330

    add ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { Config } from 'tailwindcss'
 const config: Config = {
 	content: ['./views/**/*.{ejs,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#00a1b1',
+			},
+		},
 	},
 	plugins: [],
 }
